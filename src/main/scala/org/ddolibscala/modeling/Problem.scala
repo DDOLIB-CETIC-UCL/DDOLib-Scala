8963209873ef2189fc0560ec7183a6235f470e84
qme <quentin.meurisse@cetic.be>
2026-01-06 13:30:40 +0100

    add other method to implement

--- src/main/scala/org/ddolibscala/modeling/Problem.scala
+++ src/main/scala/org/ddolibscala/modeling/Problem.scala
@@ -1,6 +1,7 @@
 package org.ddolibscala.modeling
 
-import java.util.Optional
+import org.ddolib.ddo.core.Decision
+
 import scala.jdk.CollectionConverters.IteratorHasAsJava
 import scala.jdk.OptionConverters.RichOption
 
@@ -10,13 +11,25 @@ trait Problem[T] extends org.ddolib.modeling.Problem[T] {
 
   def optimal: Option[Double] = None
 
+  override def nbVars(): Int
+
+  override def initialState(): T
+
+  override def initialValue(): Double
+
+  override def transition(state: T, decision: Decision): T
+
+  override def transitionCost(state: T, decision: Decision): Double
+
+  override def evaluate(solution: Array[Int]): Double
+
   // METHODS TO CONVERT SCALA OBJECTS INTO JAVA OBJECTS
 
-  final override def domain(state: T, variable: Int): java.util.Iterator[Integer] = {
+  final override def domain(state: T, variable: Int): java.util.Iterator[java.lang.Integer] = {
     nextValues(state, variable).map(i => java.lang.Integer.valueOf(i)).iterator.asJava
   }
 
-  final override def optimalValue(): Optional[java.lang.Double] =
+  final override def optimalValue(): java.util.Optional[java.lang.Double] =
     optimal.map(d => java.lang.Double.valueOf(d)).toJava
 
 }
