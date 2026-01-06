5ab651c969356be14a602a3b4c170d2456ac30df
qme <quentin.meurisse@cetic.be>
2026-01-06 15:27:54 +0100

    remove compilation warning

--- src/main/scala/org/ddolibscala/common/dominance/SimpleDominanceChecker.scala
+++ src/main/scala/org/ddolibscala/common/dominance/SimpleDominanceChecker.scala
@@ -1,9 +1,11 @@
 package org.ddolibscala.common.dominance
 
-import org.ddolib.common.dominance.SimpleDominanceChecker
 import org.ddolib.modeling.Dominance
 
 object SimpleDominanceChecker {
-  def apply[T](dominance: Dominance[T], nbVars: Int): SimpleDominanceChecker[T] =
-    new SimpleDominanceChecker[T](dominance, nbVars)
+  def apply[T](
+    dominance: Dominance[T],
+    nbVars: Int
+  ): org.ddolib.common.dominance.SimpleDominanceChecker[T] =
+    new org.ddolib.common.dominance.SimpleDominanceChecker[T](dominance, nbVars)
 }
--- src/main/scala/org/ddolibscala/ddo/core/frontier/SimpleFrontier.scala
+++ src/main/scala/org/ddolibscala/ddo/core/frontier/SimpleFrontier.scala
@@ -1,14 +1,19 @@
 package org.ddolibscala.ddo.core.frontier
 
-import org.ddolib.ddo.core.frontier.{CutSetType, SimpleFrontier}
 import org.ddolib.modeling.StateRanking
 
 object SimpleFrontier {
 
-  def lastExactLayer[T](ranking: StateRanking[T]): SimpleFrontier[T] =
-    new SimpleFrontier[T](ranking, CutSetType.LastExactLayer)
+  def lastExactLayer[T](ranking: StateRanking[T]): org.ddolib.ddo.core.frontier.SimpleFrontier[T] =
+    new org.ddolib.ddo.core.frontier.SimpleFrontier[T](
+      ranking,
+      org.ddolib.ddo.core.frontier.CutSetType.LastExactLayer
+    )
 
-  def apply[T](ranking: StateRanking[T]): SimpleFrontier[T] =
-    new SimpleFrontier[T](ranking, CutSetType.Frontier)
+  def apply[T](ranking: StateRanking[T]): org.ddolib.ddo.core.frontier.SimpleFrontier[T] =
+    new org.ddolib.ddo.core.frontier.SimpleFrontier[T](
+      ranking,
+      org.ddolib.ddo.core.frontier.CutSetType.Frontier
+    )
 
 }
