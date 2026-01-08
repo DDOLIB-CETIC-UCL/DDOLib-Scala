0d0ac30ebce98fb982d374984477d55922da5c30
qme <quentin.meurisse@cetic.be>
2026-01-08 15:19:59 +0100

    correct the doc

--- src/main/scala/org/ddolibscala/util/DebugMode.scala
+++ src/main/scala/org/ddolibscala/util/DebugMode.scala
@@ -12,26 +12,25 @@ enum DebugMode {
     * <p> This mode is intended for production runs or benchmarking, where performance is
     * prioritized and no additional checks or debug information are generated. </p>
     */
+  case Off
+
+  /** Enables basic debugging checks.
+    *
+    * <p> This mode verifies fundamental properties of the model components, such as:</p>
+    *
+    *   - Equality and correctness of state representations.
+    *   - Proper definition and consistency of lower bounds.
+    *
+    * These checks help detect common modeling or implementation errors early in the solving
+    * process.
+    */
   case On
 
   /** Enables extended debugging and diagnostic tools.
     *
     * <p> Includes all checks from `ON`, and adds: </p>
-    *   - Export of failing or inconsistent decision diagrams (as <code>.dot</code> files) to assist
-    *     with visualization and analysis.
-    *   - Additional consistency verification of lower-bound computations (particularly useful for
-    *     A*-based algorithms)
-    *
-    * This mode is recommended when investigating unexpected solver behavior or validating complex
-    * model implementations.
-    */
-  case Off
-
-  /** Enables extended debugging and diagnostic tools.
-    *
-    * <p> Includes all checks from `ON`, and adds: </p>
-    *   - Export of failing or inconsistent decision diagrams (as <code>.dot</code> files) to assist
-    *     with visualization and analysis.
+    *   - Export of failing or inconsistent decision diagrams (as `.dot` files) to assist with
+    *     visualization and analysis.
     *   - Additional consistency verification of lower-bound computations (particularly useful for
     *     A*-based algorithms)
     *
@@ -40,7 +39,7 @@ enum DebugMode {
     */
   case Extended
 
-  /** Converts this enum into Java enum.*/
+  /** Converts this enum into Java enum. */
   def toJava: JavaDebugLvl = {
     this match {
       case On       => JavaDebugLvl.ON
