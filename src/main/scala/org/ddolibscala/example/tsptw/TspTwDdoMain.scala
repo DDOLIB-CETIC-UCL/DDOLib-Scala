7e0b29451a880629e2c69414008b84605014ed6c
qme <quentin.meurisse@cetic.be>
2026-02-02 16:05:24 +0100

    main for tsptw

---
+++ src/main/scala/org/ddolibscala/example/tsptw/TspTwDdoMain.scala
@@ -0,0 +1,37 @@
+package org.ddolibscala
+package example.tsptw
+
+import org.ddolibscala.tools.ddo.frontier.CutSetType.Frontier
+import org.ddolibscala.tools.ddo.heuristics.width.FixedWidth
+import org.ddolibscala.tools.dominance.SimpleDominanceChecker
+import org.ddolibscala.util.DebugMode.On
+
+object TspTwDdoMain {
+
+  def main(args: Array[String]): Unit = {
+    val problem = TspTwProblem("data/TSPTW/nbNodes_4_1.txt")
+    println(problem)
+    val solver = Solver.ddo(
+      problem,
+      relaxation = TspTwRelax(problem.nbVars()),
+      lowerBound = TspTwFlb(problem),
+      dominance = SimpleDominanceChecker(TspTwDominance(), problem.nbVars()),
+      ranking = TspTwRanking(),
+      widthHeuristic = FixedWidth(100),
+      frontier = Frontier,
+      useCache = true,
+      debugMode = On
+    )
+
+    val solution = solver.minimize(onSolution = (sol, stats) => {
+      println("------ NEW BEST ------")
+      println(stats)
+      println(s"0 -> ${sol.mkString(" -> ")}")
+    })
+
+    println(solution)
+    println(s"Search time: ${solution.statistics().runTimeMs()} ms")
+
+  }
+
+}
--- src/main/scala/org/ddolibscala/example/tsptw/TspTwProblem.scala
+++ src/main/scala/org/ddolibscala/example/tsptw/TspTwProblem.scala
@@ -4,6 +4,8 @@ import org.ddolib.ddo.core.Decision
 import org.ddolibscala.modeling.Problem
 
 import scala.collection.immutable.BitSet
+import scala.io.Source
+import scala.util.Using
 
 object TspTwProblem {
   def apply(
@@ -11,17 +13,41 @@ object TspTwProblem {
     time: Int => Int => Int,
     timeWindows: Array[TimeWindow],
     optimal: Option[Double] = None
-  ): TspTwProblem = new TspTwProblem(numNodes, time, timeWindows, optimal)
+  ): TspTwProblem = new TspTwProblem(numNodes, time, timeWindows, optimal, None)
+
+  def apply(fname: String): TspTwProblem = {
+    Using.resource(Source.fromFile(fname)) { reader =>
+      val lines = reader.getLines().map(_.trim).filter(l => l.nonEmpty && !l.startsWith("#")).toList
+
+      val firstLine: Array[String]     = lines.head.split("\\s+")
+      val numVar: Int                  = firstLine(0).toInt
+      val optimalValue: Option[Double] =
+        if (firstLine.length == 2) Some(firstLine(1).toDouble) else None
+
+      val timeMatrix: Array[Array[Int]] =
+        lines.slice(1, numVar + 1).map(line => line.split("\\s+").map(_.toInt)).toArray
+
+      val tw: Array[TimeWindow] = lines
+        .slice(numVar + 1, 2 * numVar + 1)
+        .map { line =>
+          val bounds = line.split("\\s+")
+          TimeWindow(bounds(0).toInt, bounds(1).toInt)
+        }
+        .toArray
+
+      new TspTwProblem(numVar, i => j => timeMatrix(i)(j), tw, optimalValue, None)
+    }
+
+  }
 }
 
 class TspTwProblem(
   numNodes: Int,
   val time: Int => Int => Int,
   val timeWindows: Array[TimeWindow],
-  _optimal: Option[Double]
+  _optimal: Option[Double],
+  name: Option[String]
 ) extends Problem[TspTwState] {
-
-  private var name: Option[String] = None
 
   override def nbVars(): Int = numNodes
 
@@ -116,5 +142,20 @@ class TspTwProblem(
   private def arrivalTime(from: TspTwState, to: Int): Int = {
     val time: Int = from.time + minDuration(from, to)
     time max timeWindows(to).start
+  }
+
+  override def toString: String = {
+    val timeMatrixStr = Array
+      .tabulate(nbVars(), nbVars())((i, j) => time(i)(j))
+      .map(_.map(x => f"$x%3s").mkString(" "))
+      .mkString("\n")
+    val str =
+      s"""Num nodes: $numNodes
+         |Times windows: ${timeWindows.mkString(", ")}
+         |Time matrix:
+         |$timeMatrixStr
+         |""".stripMargin
+
+    name.getOrElse(str)
   }
 }
